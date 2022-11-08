baad101f5322ae6b6f04eec9b0717e322c0c94a9
许将龙 <xujl@weipaitang.com>
2022-11-08 17:11:44 +0800

    feat: 初始化脚手架

---
+++ bin/ask.js
@@ -0,0 +1,13 @@
+// 配置ask 选项
+module.exports = [
+    {
+      type: 'input',
+      name: 'author',
+      message: 'author?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'description?'
+    }
+];
---
+++ bin/http.js
@@ -0,0 +1,15 @@
+// 引入axios
+const axios = require('axios');
+
+axios.interceptors.response.use((res) => {
+  return res.data;
+});
+
+// 获取git上的项目列表
+async function getRepolist() {
+  return axios.get('https://api.github.com/orgs/yuan-cli/repos');
+}
+
+module.exports = {
+  getRepolist
+};
